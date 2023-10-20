ba0ba48d13ed6ce47258938003ab322b7f8829b9
dhananjay1002000 <55107701+dhananjay1002000@users.noreply.github.com>
2023-10-20 11:08:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,8 +8,11 @@ const port = 4000;
 app.use(cors());
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true }));
+import dotenv from 'dotenv';
+dotenv.config();
 
-mongoose.connect("mongodb+srv://admin:1234@cluster0.utkfs.mongodb.net/" ,{
+const URI = process.env.URI;
+mongoose.connect(URI ,{
   useNewUrlParser:true, 
   useUnifiedTopology:true
  }
